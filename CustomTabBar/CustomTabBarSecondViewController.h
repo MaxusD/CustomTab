8ce0c24e071592f7ec73082f63b6b1407dc987f7
Andrey Pushenko <apushenko@Andreys-MacBook-Pro.local>
2013-06-27 00:54:26 +0300

    Initial Commit

---
+++ CustomTabBar/CustomTabBarSecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  CustomTabBarSecondViewController.h
+//  CustomTabBar
+//
+//  Created by Andrey Pushenko on 6/27/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomTabBarSecondViewController : UIViewController
+
+@end
