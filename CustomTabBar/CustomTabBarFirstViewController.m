9770c2dfc32a015ca593190f87f926b3cdf4e8d6
unknown <D:\Program Files\The Bat!\Mail>
2013-07-08 12:30:16 +0300

    Background image about screen

--- CustomTabBar/CustomTabBarFirstViewController.m
+++ CustomTabBar/CustomTabBarFirstViewController.m
@@ -21,7 +21,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	[[self view] setBackgroundColor:[UIColor colorWithPatternImage:[UIImage imageNamed:@"master_bg@2x.png"]]]; 
 }
 
 - (void)viewDidUnload
